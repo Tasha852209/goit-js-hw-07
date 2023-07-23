c47ed3b54827c2523aab5d63266008c7ef49d864
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-23 16:26:54 +0200

    1+2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryUl = document.querySelector(".gallery");
+
+createGallery();
+
+function createGallery() {
+  const galleryList = galleryItems.reduce((acc, image) => {
+    return (
+      acc +
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}"
+              alt="${image.description}"
+            />
+          </a>
+        </li>`
+    );
+  }, "");
+  galleryUl.innerHTML = galleryList;
+}
+galleryUl.addEventListener("click", showModal);
+
+function showModal(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery")) {
+    return;
+  }
+  const src = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${src}"/>`);
+  instance.show();
+
+  window.addEventListener("keydown", closeOnEsc);
+  function closeOnEsc(event) {
+    if (event.code === "Escape") {
+      closeModal();
+    }
+  }
+
+  function closeModal() {
+    window.removeEventListener("keydown", closeOnEsc);
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryUl = document.querySelector(".gallery");
+createGallery();
+
+function createGallery() {
+  const galleryList = galleryItems.reduce((acc, image) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`
+    );
+  }, "");
+  galleryUl.innerHTML = galleryList;
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+lightbox.show();
 
 console.log(galleryItems);
